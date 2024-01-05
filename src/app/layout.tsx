2e3184a6948215c89a3ef20f8243a76370deba33
Ana Lambea <analambeap@gmail.com>
2024-01-05 14:46:43 +0100

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import React, { PropsWithChildren } from "react";
+import { Oswald } from "next/font/google";
+import "./globals.css";
+
+const inter = Oswald({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Userticfy user control",
+  description: "Website dedicated to control Userticfy's users",
+};
+
+interface RootLayoutProps extends PropsWithChildren {}
+
+const RootLayout = ({ children }: RootLayoutProps): React.ReactElement => {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./page.module.css";
+
+const Home = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h2 className="header__title">Userticfy</h2>
+      </header>
+      <main className={styles.main}></main>
+    </>
+  );
+};
+
+export default Home;
