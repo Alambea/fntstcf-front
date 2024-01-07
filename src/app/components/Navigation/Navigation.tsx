b8aec86913d7dadbbb528283d9ecf68834619634
Ana Lambea <analambeap@gmail.com>
2024-01-07 18:05:47 +0100

    Add Navigation component

---
+++ src/app/components/Navigation/Navigation.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import "./Navigation.scss";
+
+const Navigation = (): React.ReactElement => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="navigation">
+      <ul className="navigation__link-list">
+        <li>
+          <Link
+            className={`navigation__link ${pathname === "/" ? "active" : ""}`}
+            href="/"
+          >
+            Users
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`navigation__link ${
+              pathname === "/add-user" ? "active" : ""
+            }`}
+            href="/add-user"
+          >
+            Add User
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
