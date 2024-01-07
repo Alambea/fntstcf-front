766a302a82e86e4a9a3110bd34bae8bd7b9660d2
Ana Lambea <analambeap@gmail.com>
2024-01-07 14:08:29 +0100

    Add Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import styles from "./page.module.scss";
-
 const Home = () => {
-  return (
-    <>
-      <header className={styles.header}>
-        <h2 className="header__title">Userticfy</h2>
-      </header>
-      <main className={styles.main}></main>
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
