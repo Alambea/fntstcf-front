0476b535dae94c8b2d9972a8dca9c23b27dbb926
Ana Lambea <analambeap@gmail.com>
2024-01-08 14:45:14 +0100

    Add msw testing configuration

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import { handlers } from "./app/mocks/handlers";
+import { server } from "./app/mocks/node";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers(...handlers));
+afterAll(() => server.close());
