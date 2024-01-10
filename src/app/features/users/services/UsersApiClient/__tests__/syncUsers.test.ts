85784167b695272114438763156de66635d8682d
Ana Lambea <analambeap@gmail.com>
2024-01-10 18:45:23 +0100

    Test UsersApiClient's method syncUsers test unit

---
+++ src/app/features/users/services/UsersApiClient/__tests__/syncUsers.test.ts
@@ -0,0 +1,15 @@
+import { usersMock } from "../../../../../mocks/usersMock";
+import UsersApiClient from "../UsersApiClient";
+
+describe("Given a UsersApiClient's method syncUsers", () => {
+  describe("When it is called succesfully", () => {
+    test(`Then it should return a list of users '${usersMock[0].name}' and '${usersMock[1].name}'`, async () => {
+      const expectedUsers = usersMock;
+      const userApiClient = new UsersApiClient();
+
+      const users = await userApiClient.syncUsers();
+
+      expect(users).toStrictEqual(expectedUsers);
+    });
+  });
+});
